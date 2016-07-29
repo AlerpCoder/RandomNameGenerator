5e8d68e13e8e37259575372686e9f07f21332875
AlerpCoder <AlerpCoder@users.noreply.github.com>
2016-07-29 12:17:24 +0200

    3. Version you can Select between Boy or Girl in the command line but it dosent match the right gender.

--- src/RandomNameGenerator.java
+++ src/RandomNameGenerator.java
@@ -4,10 +4,17 @@ import java.util.*;
 
 public class RandomNameGenerator {
     private static HashSet<String> names = new HashSet<>();
+    private static boolean isBoy=false;
 
     private static void namesParser() throws IOException {
+        String maleFemale;
+        if(isBoy){
+            maleFemale="jungennamen";
+        }else{
+            maleFemale="maedchennamen";
+        }
         for (int letter = 97; letter < 123; letter++) {
-            String html = "http://www.vornamen.ch/namensliste/" + letter + ".html";
+            String html = "http://www.vornamen.ch/"+maleFemale+"/" + letter + ".html";
             String lastPage = (Jsoup.connect(html).get()).
                     select("a[title*=Zur letzten Seite]").attr("href");
 
@@ -18,9 +25,11 @@ public class RandomNameGenerator {
                 end = 1;
             }
             for (int j = 1; j <= end; j++) {
-                String newHtml = "http://www.vornamen.ch/namensliste/" + letter + "," + j + ".html";
-                strtok((Jsoup.
-                        connect(newHtml).get()).
+                String newHtml = "http://www.vornamen.ch/"+maleFemale+"/" + letter + "," + j + ".html";
+                strtok((Jsoup.connect(newHtml).get()).
+                        removeClass("jungencharts").
+                        removeClass("maedchencharts").
+                        removeClass("trend").
                         select("a[href*=../name/]").text());
             }
         }
@@ -41,7 +50,36 @@ public class RandomNameGenerator {
         return namen.get(new Random().nextInt(names.size()));
     }
 
+    private static void userInteraction() {
+        Scanner input= new Scanner(System.in);
+        System.out.println("Schould the name for a girl (girl, g, m,) or a boy (boy, b, j)? (Please insert on word in from the brackets)");
+        String gender= input.next();
+        genderDecider(gender);
+    }
+
+    private static void genderDecider(String gender) {
+        switch (gender) {
+            case "m":
+            case "g":
+            case "girl":
+                isBoy = false;
+                break;
+            case "j":
+            case "b":
+            case "boy":
+                isBoy = true;
+                break;
+            default:
+                System.out.println("Please insert a valid word or letter in the input box");
+                userInteraction();
+                break;
+        }
+    }
+
     public static void main(String[] args) throws IOException {
+        userInteraction();
         System.out.println(randomNameGenerator());
     }
+
+
 }
